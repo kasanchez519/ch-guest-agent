836d10e4b72421b33d5e23cb015a15b07862a164
Karissa Sanchez <t-karsanchez@microsoft.com>
2023-07-28 13:46:23 -0700

    flake8 fixes
    Signed-off-by: Karissa Sanchez <t-karsanchez@microsoft.com>

--- misc/client.py
+++ misc/client.py
@@ -1,12 +1,11 @@
 import socket
-import json
 # from qemu.qmp import Message
 
 PORT = 1234
-# cmd = '{"execute": "create-user", "arguments": {"username": "testuser", "create-home": 1} }'
-# cmd = '{"execute": "deploy-ssh-pubkey", "arguments": {"username": "testuser", "ssh-key": "test_ssh_key"} }'
+cmd = '{"execute": "create-user", "arguments": {"username": "testuser", "create-home": 1} }' # noqa E501
+cmd = '{"execute": "deploy-ssh-pubkey", "arguments": {"username": "testuser", "ssh-key": "test_ssh_key"} }' # noqa E501
 cmd = '{"execute": "get-osinfo"}'
-# cmd = '{"execute": "guest-sync", "arguments": {"id": 12345} }'
+cmd = '{"execute": "guest-sync", "arguments": {"id": 12345} }'
 
 # print(cmd)
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -16,5 +15,3 @@ s.send("CONNECT {}\n{}".format(PORT, cmd).encode('utf-8'))
 buf1 = s.recv(4096)
 buf2 = s.recv(4096).decode('utf-8')
 print(buf2)
-
-
