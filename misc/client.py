8f75558f04ad5f0d5880e9bcff0b086297e68dc4
Karissa Sanchez <t-karsanchez@microsoft.com>
2023-07-28 13:43:01 -0700

    send and recieve commands on client
    Signed-off-by: Karissa Sanchez <t-karsanchez@microsoft.com>

--- misc/client.py
+++ misc/client.py
@@ -1,12 +1,20 @@
 import socket
+import json
+# from qemu.qmp import Message
 
 PORT = 1234
+# cmd = '{"execute": "create-user", "arguments": {"username": "testuser", "create-home": 1} }'
+# cmd = '{"execute": "deploy-ssh-pubkey", "arguments": {"username": "testuser", "ssh-key": "test_ssh_key"} }'
+cmd = '{"execute": "get-osinfo"}'
+# cmd = '{"execute": "guest-sync", "arguments": {"id": 12345} }'
 
+# print(cmd)
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.connect("/tmp/ch.vsock")
-s.send("CONNECT {}\nHello, world!".format(PORT).encode('utf-8'))
+s.send("CONNECT {}\n{}".format(PORT, cmd).encode('utf-8'))
 
-buf = s.recv(64)
-buf = buf.decode('utf-8')
-print(buf)
-s.close()
+buf1 = s.recv(4096)
+buf2 = s.recv(4096).decode('utf-8')
+print(buf2)
+
+
