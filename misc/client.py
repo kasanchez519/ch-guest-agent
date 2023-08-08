53cc972bb1a93353e2736e730ec6f1a51fac992c
Karissa Sanchez <t-karsanchez@microsoft.com>
2023-08-08 12:52:54 -0700

    create cli parser
    Signed-off-by: Karissa Sanchez <t-karsanchez@microsoft.com>

--- misc/client.py
+++ misc/client.py
@@ -1,16 +1,10 @@
 import socket
 import qmp_formatter
 from client_parser import build_parser
-# from qemu.qmp import Message
+
 cmd = build_parser()
 print(cmd)
 PORT = 1234
-# cmd = '{"execute": "create-user", "arguments": {"username": "testuser", "create-home": 1} }' # noqa E501
-# cmd = '{"execute": "deploy-ssh-pubkey", "arguments": {"username": "testuser", "ssh-key": "test_ssh_key"} }' # noqa E501
-# cmd = '{"execute": "get-osinfo"}'
-# cmd = '{"execute": "guest-sync", "arguments": {"id": 12345} }'
-
-# print(cmd)
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.connect("/tmp/ch.vsock")
 s.send("CONNECT {}\n{}".format(PORT, cmd).encode('utf-8'))
@@ -18,3 +12,5 @@ s.send("CONNECT {}\n{}".format(PORT, cmd).encode('utf-8'))
 buf1 = s.recv(4096)
 buf2 = s.recv(4096).decode('utf-8')
 print(buf2)
+
+s = 'CONNECT 1234\n{"execute": "deploy-ssh-pubkey", "arguments": {"username": "testuser", "ssh-key": "$(cat ~/.ssh/id_rsa.pub)"}}'
